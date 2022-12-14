e54984dd259b2a5a9a9ed6b195854934fcc20da9
Natiba <72468185+Natiba@users.noreply.github.com>
2022-12-14 14:58:26 -0300

    merbers component

--- src/components/indiceComp/Members.jsx
+++ src/components/indiceComp/Members.jsx
@@ -1,27 +1,31 @@
 import React from "react";
-import '../../style/inicio/members.css'
+import Footer from "../indiceComp/Footer";
+import "../../style/inicio/members.css";
 
 const Members = () => {
-    return (
-        <div className="members-container">
-           <div className="member">
-            <h3>Natalia Bartolomé</h3>
-            <p>Home</p>
-            </div>
-            <div className="member">
-            <h3>Ana María Zuluaga Vazquez</h3>
-            <p>Categories</p>
-            </div>
-            <div className="member">
-            <h3>Nancy </h3>
-            <p>About Us</p>
-            </div>
-            <div className="member">
-            <h3>Omar</h3>
-            <p></p>
-            </div>
+  return (
+    <>
+      <div className="members-container">
+        <div className="member">
+          <h3>Natalia Bartolomé</h3>
+          <p>Home - Members</p>
         </div>
-    )
-}
+        <div className="member">
+          <h3>Omar Ignacio Barboza</h3>
+          <p>Featured - Routeo</p>
+        </div>
+        <div className="member">
+          <h3>Ana María Zuluaga Vazquez</h3>
+          <p>Categories</p>
+        </div>
+        <div className="member">
+          <h3>Nancy Barea</h3>
+          <p>About Us</p>
+        </div>
+      </div>
+      <Footer creator="©2022 -  Created by GROUP 8 with  ❤️" />
+    </>
+  );
+};
 
-export default Members
+export default Members;
--- src/routes/Miembros.jsx
+++ src/routes/Miembros.jsx
@@ -1,12 +1,10 @@
 import React from "react";
 import Members from "../components/indiceComp/Members";
-import Footer from "../components/indiceComp/Footer";
 
 const Miembros = () => {
   return (
     <>
       <Members />
-      <Footer />
     </>
   );
 };
