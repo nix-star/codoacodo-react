d17cc90e26a2c2158a775e80a92183e73c606964
Omar <onacho83@gmail.com>
2022-12-13 23:47:02 -0300

    Grid Destacados

--- src/components/LibroCard/LibroCard.jsx
+++ src/components/LibroCard/LibroCard.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import style from './libroCard.module.css'
 
 const LibroCard = ({libro}) => {
+  let imgURL= `https://covers.openlibrary.org/b/id/${libro?.cover_i}-M.jpg`;
+
   return (
-    <li>
-      {libro}
+    <li className={style.librosCard}>
+      <img className={style.libroImage}
+      width={230}
+      height={345} src={imgURL} alt={libro.title} />
+      <div>{libro.title}</div>
     </li>
   )
 }
