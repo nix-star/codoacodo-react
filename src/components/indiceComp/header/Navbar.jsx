28cac97147adca10721bb4479a8924eacf3b4199
Natiba <72468185+Natiba@users.noreply.github.com>
2022-12-15 22:51:54 -0300

    my tp

--- src/components/indiceComp/header/Navbar.jsx
+++ src/components/indiceComp/header/Navbar.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import styled from "styled-components";
+//import styled from "styled-components";
 import BurgerButton from "./BurguerButton"
 
 const Navbar = () => {
   return (
     <>
-    <NavBarStyle>
+    
       <nav>
-        <ul className="links">
+        <ul className="links active">
           <li>
             <Link to={"/"}>Home</Link>
           </li>
@@ -32,20 +32,49 @@ const Navbar = () => {
           </li>
         </ul>
       </nav>
+      {/*
       <div className="burger">
       <BurgerButton />
-      </div>
-      </NavBarStyle>
+  </div> */}
+      
     </>
   );
 };
 
 export default Navbar;
 
+/*
+
 const NavBarStyle = styled.nav`
+
+.links {
+   position: absolute;
+   top: -700px;
+   left: -2000px;
+   margin-left: auto;
+   margin-right: auto;
+   text-align: center;
+    @media(min-width: 768px) {
+        position: initial;
+    }
+}
+
+.links.active {
+    width: 100%;
+    display: block;
+    position: absolute;
+    margin-left: auto;
+    margin-right: auto;
+    top: 30%;
+    left: 0;
+    right: 0;
+    text-align: center
+}
+
 .burger {
     @media(min-width: 768px) {
         display: none;
     }
 }
 `
+*/
